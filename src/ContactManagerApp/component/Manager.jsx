d7bb92dee80127ed808c20113ec0e5b1f7c198ff
Tolani <110635435+Babanfada@users.noreply.github.com>
2022-10-31 08:43:55 +0000

    building

--- src/ContactManagerApp/component/Manager.jsx
+++ src/ContactManagerApp/component/Manager.jsx
@@ -36,6 +36,7 @@ const Manager = () => {
     }
   };
   //NB shortcircut operator evaluate to truthy/falsy value without setting to state
+  
   const handleDelete = (index) => {
     let dataArrayCopy = [...dataArray].filter((copy, idx) => {
       return index !== idx;
